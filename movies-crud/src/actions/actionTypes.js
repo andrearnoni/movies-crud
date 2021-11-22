90e927b27a63ca7c4b120bd308d02ef46aac79f1
André Arnoni <andre_arnoni@yahoo.com.br>
2021-11-22 04:57:42 -0300

    adicionados action types para cada ação do crud

---
+++ movies-crud/src/actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const GET_MOVIES = 'GET_MOVIES';
+export const GET_MOVIE = 'GET_MOVIE';
+export const DELETE_MOVIE = 'DELETE_MOVIE';
+export const ADD_MOVIE = 'ADD_MOVIE';
+export const GET_SINGLE_MOVIE = 'GET_SINGLE_MOVIE';
+export const UPDATE_MOVIE = 'UPDATE_MOVIE';
