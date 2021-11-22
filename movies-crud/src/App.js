47d415f35a029dae07805d244f33eb031616078f
André Arnoni <andre_arnoni@yahoo.com.br>
2021-11-22 04:45:59 -0300

    config inicial de paginas e rotas

--- movies-crud/src/App.js
+++ movies-crud/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/movie/new" component={ NewMovie } />
+        <Route exact path="/movie/:id" component={ MovieDetails } />
+        <Route exact path="/movie/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+      </Switch>
     </div>
   );
 }
